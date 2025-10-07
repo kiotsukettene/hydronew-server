0759ccbf5c45e10b25804fa339038c27d7d804c4
kiotsukettene <kiotsuketteneloreto@gmail.com>
2025-10-08 00:46:53 +0800

    feat: Add seeders for various database entities
    added:
    - DevicesSeeder.php
    - HydroponicSetupSeeder.php
    - HydroponicYieldSeeder.php
    - NotificationSeeder.php
    - SensorReadingsSeeder.php
    - SensorsSeeder.php
    - TipsSuggestionsSeeder.php
    - TreatmentReportsSeeder.php
    - TreatmentStagesSeeder.php
    - UsersSeeder.php

---
+++ database/seeders/SensorsSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SensorsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
