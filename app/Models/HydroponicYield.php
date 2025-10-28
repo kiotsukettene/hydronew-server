74dd4818c7c6c85391af64c74e7b84dc2dd885e9
Raymond Palomares <palomares.raymond.rivamonte@gmail.com>
2025-10-28 22:04:44 +0800

    Yield Updates

--- app/Models/HydroponicYield.php
+++ app/Models/HydroponicYield.php
@@ -53,6 +53,6 @@ class HydroponicYield extends Model
 
     public function hydroponic_setup()
     {
-        return $this->belongsTo(HydroponicSetup::class, 'id');
+        return $this->belongsTo(HydroponicSetup::class, 'hydroponic_setup_id');
     }
 }
