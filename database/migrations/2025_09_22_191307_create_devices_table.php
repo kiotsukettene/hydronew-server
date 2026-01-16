e84be9d580179361137a2c948e1304e3549b3209
kiotsukettene <kiotsuketteneloreto@gmail.com>
2026-01-17 02:35:48 +0800

    Fix device table name

--- database/migrations/2025_09_22_191307_create_devices_table.php
+++ database/migrations/2025_09_22_191307_create_devices_table.php
@@ -13,8 +13,7 @@ return new class extends Migration
     {
         Schema::create('devices', function (Blueprint $table) {
             $table->bigInteger('id', true);
-            $table->bigInteger('user_id')->index('user_id');
-            $table->string('name', 150);
+            $table->string('device_name', 150);
             $table->string('serial_number', 150)->unique('serial_number');
             $table->string('model', 100)->nullable();
             $table->string('firmware_version', 50)->nullable();
