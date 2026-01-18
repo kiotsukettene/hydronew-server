74d513405bb1a5ad25c7be4075c64d9e42aa9722
kiotsukettene <kiotsuketteneloreto@gmail.com>
2026-01-18 20:37:19 +0800

    Allowed all origins in cors

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'broadcasting/auth', 'v1/broadcasting/auth'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:8081', 'https://api.ddev.site'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
