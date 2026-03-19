70270c92e0520cef08da95ecd5be6efd642a2974
kiotsukettene <kiotsuketteneloreto@gmail.com>
2026-03-19 09:27:09 +0800

    Update Download  QR

--- resources/js/Pages/Download.jsx
+++ resources/js/Pages/Download.jsx
@@ -46,19 +46,7 @@ const Download = () => {
                 <div className="flex flex-col sm:flex-row gap-4">
                 
 
-                  {/* Google Play Button */}
-                  <a
-                    href="#"
-                    className="flex items-center justify-center gap-3 bg-black text-white px-6 py-4 rounded-full hover:bg-gray-800 transition-all duration-300 shadow-lg hover:shadow-xl group min-w-[200px]"
-                  >
-                    <svg className="w-8 h-8" viewBox="0 0 24 24" fill="currentColor">
-                      <path d="M3,20.5V3.5C3,2.91 3.34,2.39 3.84,2.15L13.69,12L3.84,21.85C3.34,21.6 3,21.09 3,20.5M16.81,15.12L6.05,21.34L14.54,12.85L16.81,15.12M20.16,10.81C20.5,11.08 20.75,11.5 20.75,12C20.75,12.5 20.53,12.9 20.18,13.18L17.89,14.5L15.39,12L17.89,9.5L20.16,10.81M6.05,2.66L16.81,8.88L14.54,11.15L6.05,2.66Z" />
-                    </svg>
-                    <div className="text-left">
-                      <div className="text-xs opacity-90">GET IT ON</div>
-                      <div className="text-lg font-semibold">Google Play</div>
-                    </div>
-                  </a>
+               
                 </div>
 
                 {/* Divider */}
@@ -73,7 +61,7 @@ const Download = () => {
                   <div className="flex flex-col sm:flex-row items-center gap-6">
                     <div className="bg-white p-3 rounded-xl border-2 border-gray-200">
                       <img 
-                        src="/images/mock-qr.png" 
+                        src="/images/preview.jpg" 
                         alt="QR Code" 
                         className="w-32 h-32 sm:w-40 sm:h-40"
                       />
